0b7d792380cf6fc4ed4fe1c6f3a44193546aac33
Eternity0 <eternity211@126.com>
2025-07-25 23:17:11 +0800

    Personalize the pages

--- src/config.ts
+++ src/config.ts
@@ -8,21 +8,21 @@ import type {
 import { LinkPreset } from "./types/config";
 
 export const siteConfig: SiteConfig = {
-	title: "Fuwari",
-	subtitle: "Demo Site",
-	lang: "en", // 'en', 'zh_CN', 'zh_TW', 'ja', 'ko', 'es', 'th'
+	title: "Etr0的秘密花园",
+	subtitle: "Eternity0's blog",
+	lang: "zh_CN", // 'en', 'zh_CN', 'zh_TW', 'ja', 'ko', 'es', 'th'
 	themeColor: {
-		hue: 250, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
+		hue: 285, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
 		fixed: false, // Hide the theme color picker for visitors
 	},
 	banner: {
-		enable: false,
-		src: "assets/images/demo-banner.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
-		position: "center", // Equivalent to object-position, only supports 'top', 'center', 'bottom'. 'center' by default
+		enable: true,
+		src: "assets/images/blog.jpg", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+		position: "top", // Equivalent to object-position, only supports 'top', 'center', 'bottom'. 'center' by default
 		credit: {
-			enable: false, // Display the credit text of the banner image
-			text: "", // Credit text to be displayed
-			url: "", // (Optional) URL link to the original artwork or artist's page
+			enable: true, // Display the credit text of the banner image
+			text: "青葉もち - 纱露朵 宙天", // Credit text to be displayed
+			url: "https://www.pixiv.net/artworks/129217603", // (Optional) URL link to the original artwork or artist's page
 		},
 	},
 	toc: {
@@ -53,9 +53,9 @@ export const navBarConfig: NavBarConfig = {
 };
 
 export const profileConfig: ProfileConfig = {
-	avatar: "assets/images/demo-avatar.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
-	name: "Lorem Ipsum",
-	bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+	avatar: "assets/images/profile_photo(1MB).jpg", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+	name: "Eternity0w0",
+	bio: "给岁月以文明, 还是给文明以岁月? 不如给咖啡以续杯...",
 	links: [
 		{
 			name: "Twitter",
@@ -72,7 +72,7 @@ export const profileConfig: ProfileConfig = {
 		{
 			name: "GitHub",
 			icon: "fa6-brands:github",
-			url: "https://github.com/saicaca/fuwari",
+			url: "https://github.com/eternity0xd3",
 		},
 	],
 };
--- src/content/spec/about.md
+++ src/content/spec/about.md
@@ -1,9 +1,32 @@
 # About
-This is the demo site for [Fuwari](https://github.com/saicaca/fuwari).
 
-::github{repo="saicaca/fuwari"}
+## **Hi There~**
 
-> ### Sources of images used in this site
-> - [Unsplash](https://unsplash.com/)
-> - [星と少女](https://www.pixiv.net/artworks/108916539) by [Stella](https://www.pixiv.net/users/93273965)
-> - [Rabbit - v1.4 Showcase](https://civitai.com/posts/586908) by [Rabbit_YourMajesty](https://civitai.com/user/Rabbit_YourMajesty)
+This is Eternity0 !
+
++ Senior high school student
+
++ Learning: Python, Java, JS
+
++ games: **Rhythm games** are the best games of the world!!!!!!!!
+
+**I'll try to....**
+
++ design my own web blog style.
+ (Instead of applying other's themes directly!)
++ learn C++
++ work on some full-stack web projects ~~(did I translate it correctly?)~~
+
+[Go and visit my Github homepage!](https://github.com/Eternity0xd3)
+
+[Go and visit my Bilibili homepage!](https://space.bilibili.com/488842102)
+
+[Go and enjoy my favourite musics](https://music.163.com/#/playlist?id=4974517819)
+
+## Blog
+The blog is powered by [Astro](astro.build) & [Fuwari](https://github.com/saicaca/fuwari)
+
+I plan to design my own theme in the future
+
+(but not now because I can't.  wwww)
+
